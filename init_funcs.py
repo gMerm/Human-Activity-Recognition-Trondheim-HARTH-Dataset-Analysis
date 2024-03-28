fada18c053d12f6e81116f36a78681ed155b6d0f
CHRYSOVALANTIS MICHAIL LIAKOPOULOS <chrliakopoulos@outlook.com>
2024-03-28 12:32:52 +0200

    Created a module that we can use to store secondary/initial function for cleaner code

---
+++ init_funcs.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+
+#Default path to the dataset
+default_path = os.getcwd() + "/harth/"
+
+#Label dictionary: Connects the Label number to the activity
+label_dict = {
+    1: "Walking",
+    2: "Running",
+    3: "Shuffling",
+    4: "Stairs (Ascending)",
+    5: "Stairs (Descending)",
+    6: "Standing",
+    7: "Sitting",
+    8: "Lying",
+    13: "Cycling (sit)",
+    14: "Cycling (Stand)",
+    130: "Cycling (sit, inactive)",
+    140: "Cycling (stand, inactive)"
+}
+
+
+# This Function lists all the files in the directory
+def list_files(path=default_path):
+    if(path is None):
+        path = os.getcwd() + "/harth"
+    files = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            files.append(file)
+    return files
+
+# This Function reads the files and returns the data
+def read_files(files,path=default_path):
+    data = []
+    for file in files:
+        file = path + file
+        data.append(pd.read_csv(file))
+    return data
+
+# This Function returns the showing percentage of the labels
+def get_label_stats(frame):
+    label_count = frame['label'].value_counts()
+    percentages = label_count*100 / len(frame)
+    print("Percentages of each label:")
+    print(percentages)
+
+    
+    
