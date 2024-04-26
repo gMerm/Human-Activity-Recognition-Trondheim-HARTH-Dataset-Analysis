2dcef911f6b51d668435edc748eb65ce3c0614ca
CHRYSOVALANTIS MICHAIL LIAKOPOULOS <chrliakopoulos@outlook.com>
2024-04-26 22:42:07 +0300

    Better code on NN

---
+++ delivery/neural_networks/init_funcs.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+
+#Default path to the dataset
+default_path = "../../harth/"
+
+#Label dictionary: Connects the Label number to the activity
+label_dict = {
+    1: "Walking",
+    2: "Running",
+    3: "Shuffling",
+    4: "Stairs (Ascending)",
+    5: "Stairs (Descending)",
+    6: "Standing",
+    7: "Sitting",
+    8: "Lying",
+    13: "Cycling (sit)",
+    14: "Cycling (Stand)",
+    130: "Cycling (sit, inactive)",
+    140: "Cycling (stand, inactive)"
+}
+
+
+# This Function lists all the files in the directory
+def list_files(path=default_path):
+    if(path is None):
+        path = os.getcwd() + "/harth"
+    files = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            files.append(file)
+    return files
+
+# This Function reads the files and returns the data
+def read_files(files: list,path=default_path):
+    data = []
+    for file in files:
+        file = path + file
+        data.append(pd.read_csv(file))
+    return data
+
+# This Function fixes the labels to match numerically the activities
+def fix_labels(data: pd.DataFrame):
+    data['label'] = data['label'].replace(13, 9)
+    data['label'] = data['label'].replace(14, 10)
+    data['label'] = data['label'].replace(130, 11)
+    data['label'] = data['label'].replace(140, 12)
+    return data
+
+
+# This Function returns the showing percentage of the labels
+def get_label_stats(frame: pd.DataFrame):
+    label_count = frame['label'].value_counts()
+    percentages = label_count*100 / len(frame)
+    print("Percentages of each label:")
+    print(percentages)
+
+def drop_columns(data: list):
+    for i in range(len(data)):
+        data[i]=data[i].dropna()
+        data[i]=data[i].drop(columns=['timestamp'])
+        if "Unnamed: 0" in data[i].columns:
+            data[i]=data[i].drop(columns=['Unnamed: 0'])
+        if "index" in data[i].columns:
+            data[i]=data[i].drop(columns=['index'])
+    return data
+    
+    
--- delivery/neural_networks/nn_tensor.py
+++ delivery/neural_networks/nn_tensor.py
@@ -1,73 +1,93 @@
-from init_funcs import *
-import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import regularizers
 import numpy as np
+import tensorflow as tf
+from init_funcs import *
+from tensorflow.keras import layers, Input, regularizers, Sequential # type: ignore # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping # type: ignore
+from tensorflow.keras.losses import SparseCategoricalCrossentropy # type: ignore
+from tensorflow.keras.layers import Dense, LeakyReLU # type: ignore
 from sklearn.metrics import *
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.callbacks import EarlyStopping
 
 
-def fix_labels(data):
-    #Fix labels to shrink the number of classes
-    data['label'] = data['label'].replace(13, 9)
-    data['label'] = data['label'].replace(14, 10)
-    data['label'] = data['label'].replace(130, 11)
-    data['label'] = data['label'].replace(140, 12)
-    return data
-
-
-
-
-print("Neural Network Classifier - Tensorflow")
-print("Loading Data")
-files = list_files()
-files.sort()
-print("Total Number of files:", len(files))
-data = read_files(files)
-data = drop_columns(data)
-print("Data Loaded")
-
-train_data = pd.concat(data)
-train_data = fix_labels(train_data)
-train_columns = train_data.iloc[:, 0:len(data[0].columns)]
-train_labels = train_data['label']
-
-train_data, test_data, train_labels, test_labels = train_test_split(train_columns, train_labels, test_size=0.3,
-                                                                    random_state=None, shuffle=False)
-data = pd.DataFrame()
-metric_df = pd.DataFrame(columns=['Accuracy', 'Precision', 'Recall', 'F-Score'])
-
-# Training phase of model 2
-
-model = tf.keras.Sequential([
-        tf.keras.Input(shape=(train_data.shape[1],)),
-        #layers.Dense(6, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=0.01, l2=0.01)),
-        layers.Dense(24, activation='relu'),
-        layers.Dense(24, activation='relu', kernel_regularizer=regularizers.l1(0.004)),
-        layers.Dense(13, activation='softmax', kernel_regularizer=regularizers.l1(0.004))
+# Function to create a model
+# The model has 2 hidden layers with 24 neurons each and a L1 regularization of 0.2
+def create_model():
+    model = Sequential([
+        Input(shape=(train_data.shape[1],)),
+        Dense(24, activation=LeakyReLU() , kernel_regularizer=regularizers.l1(0.2)),
+        Dense(24, activation=LeakyReLU(), kernel_regularizer=regularizers.l1(0.2)),
+        Dense(13, activation='softmax')
     ])
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1, restore_best_weights=True)
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss=SparseCategoricalCrossentropy(),
+                  metrics=['sparse_categorical_accuracy'])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-              metrics=[
-                  tf.keras.metrics.SparseCategoricalAccuracy()
-                ])
+    return model
 
-history = model.fit(train_data, train_labels, epochs=10, validation_split=0.2, callbacks=[early_stopping])
+# Function to train and test a model
+# The model is trained for 10 epochs with a validation split of 0.18
+def train_test_model():
+    # Early stopping with patience of 2 and monitor on validation loss
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
-# Make predictions
-predictions = model.predict(test_data)
+    # Train the models
+    model.fit(train_data, train_labels, epochs=10, validation_split=0.18, callbacks=[early_stopping], verbose=1)
 
-# Convert predictions to label indices
+    # Make predictions
+    predictions = model.predict(test_data)
+    predicted_labels = np.argmax(predictions, axis=1)
 
-predicted_labels = np.argmax(predictions, axis=1)
-model.save('neural_network__ReLU_4layers_24neurons_10epochs.h5')
+    # model.save('neural_network__ReLU_4layers_24neurons_10epochs.keras')
 
-accuracy = accuracy_score(test_labels, predicted_labels)
-precision = precision_score(test_labels, predicted_labels, average='macro', zero_division=0)
-recall = recall_score(test_labels, predicted_labels, average='macro', zero_division=0)
-fScore = f1_score(test_labels, predicted_labels, average='macro', zero_division=0)
-print(f"\nResults:\nAccuracy: {accuracy} Precision: {precision} Recall: {recall} F-Score: {fScore}\n")
+    # Metrics
+    accuracy = accuracy_score(test_labels, predicted_labels)
+    precision = precision_score(test_labels, predicted_labels, average='macro', zero_division=0)
+    recall = recall_score(test_labels, predicted_labels, average='macro', zero_division=0)
+    fScore = f1_score(test_labels, predicted_labels, average='macro', zero_division=0)
+    print(f"\nResults:\nAccuracy: {accuracy} Precision: {precision} Recall: {recall} F-Score: {fScore}\n")
+
+# Main Function 
+print("Neural Network Classifier - Tensorflow")
+print("Loading Data")
+
+files = list_files()
+files.sort()
+
+print("Total Number of files:", len(files))
+print("Preprocessing Data")
+
+data = read_files(files)
+data = drop_columns(data)
+data = pd.concat(data) # concatenate all dataframes to one for train test split
+data = fix_labels(data)
+get_label_stats(data)
+
+print("Data Loaded and Preprocessed")
+
+train_data = data.iloc[:, 0:len(data.columns)]
+train_labels = data['label']
+train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size=0.3,
+                                                         random_state=42, shuffle=True)
+data = pd.DataFrame() # clear memory
+model = create_model()
+train_test_model()
+
+
+
+
+
+# model = KerasRegressor(build_fn=create_model, verbose=0)
+# param_grid = {{
+#     'num_neurons': [12, 24, 32],
+#     'learning_rate': [0.001, 0.01, 0.1],
+#     'momentum': [0.0, 0.2, 0.4],
+
+# }}
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+# grid_result = grid.fit(train_data, train_labels)
+
+# # Print results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+
